416ca812e4c6e5f0a28d62a439d67fe26e62b3fc
VishwambharKachave <94899513+VishwambharKac@users.noreply.github.com>
2024-06-06 18:32:42 +0000

    camparison

---
+++ basic.js
@@ -0,0 +1,38 @@
+//In JavaScript, var, let, and const are keywords used to declare variables,
+//but they have different characteristics and scoping rules. 
+// const acctId =  1234
+// let acctEmail = "vishukac22@gmail"
+// var pasword = "2344"
+// console.log(acctId);
+
+//DATATYPES -
+// let name = "Vishwambhar" //string
+// let age = 23 //number
+// let isloggedIn = false  //boolean
+
+// Primitive datatypes=
+// number = 2 to the power of 53
+// string = in string we can use single / double cot
+// boolean = true or false
+// bigint = use for large value than number
+// null = standalone value
+// undefined = the value is not define
+// sysbol = used for unique component
+
+// console.log(typeof age) //used to get the type of datatype 
+
+/*************************************************************************************** */
+
+//VALUE CONVERSION-
+
+// let value = 12
+// let negValue = -(value)
+// console.log(negValue)
+
+//CAMPARISON-
+//  console.log(2 < 4)
+//  console.log(2 > 4)
+//  console.log(2 <= 4)
+//  console.log(2 >= 4)
+//  console.log(2 == 4)
+//  console.log(2 != 4)
